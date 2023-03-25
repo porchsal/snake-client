a41987d446c135a17d7639be0f08111d4f1313b3
porchsal <porchsal@yahoo.com>
2023-03-25 15:50:30 -0400

    play.js connectin message

--- play.js
+++ play.js
@@ -7,11 +7,19 @@ const connect = function () {
         port : 50541,
     });
 // Interpret incoming data as text
-
     conn.setEncoding("utf8");
-
+    conn.on("connect", () => {
+        conn.write("Name: JSE");
+    })
+    conn.on("connect", () => {
+        
+        console.log("Successfully connected to game server");
+    })
+    
     return conn;
 };
 
 console.log("Connecting....");
 connect();
+
+module.exports = connect;
