ec97947a9da8cd89dcd42e71fa92c043f144d94a
porchsal <porchsal@yahoo.com>
2023-03-26 01:32:49 -0400

    input.js constants

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
-let connection;
+const { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, MESSAGE } = require("./constants");
 
+let connection;
+//function to catch keyboard
 const setupInput = function (conn) {
     connection = conn;
     const stdin = process.stdin;
@@ -11,20 +13,20 @@ const setupInput = function (conn) {
     return stdin;
   };
 
-
+//movements sent by user through keyboard
 const handleUserInput = function(key){
     if (key === '\u0003') {
         process.exit();
-      } else if (key === 'w'){
+      } else if (key === MOVE_UP){
         connection.write("Move: up");
-      } else if ( key === 's'){
+      } else if ( key === MOVE_DOWN){
         connection.write("Move: down");
-      } else if ( key === '\a'){
+      } else if ( key === MOVE_LEFT){
         connection.write("Move: left");
-      } else if ( key === 'd'){
+      } else if ( key === MOVE_RIGHT){
         connection.write("Move: right");
-      } else if ( key === 'm'){
-        console.log("Snakesssssss");
+      } else if ( key === MESSAGE){
+        console.log("SNAKESSSSSS!")
       }
 };
 
