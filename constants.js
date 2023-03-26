312c61d3d0ec15c19ceaad56f54edb65c20f01c7
porchsal <porchsal@yahoo.com>
2023-03-26 01:33:36 -0400

    constants.js

---
+++ constants.js
@@ -0,0 +1,19 @@
+//constants for the game
+
+const IP = 'localhost';
+const PORT = 50541;
+const MOVE_UP = 'w';
+const MOVE_DOWN = 's';
+const MOVE_LEFT = 'a';
+const MOVE_RIGHT = 'd';
+const MESSAGE = 'm';
+
+module.exports = {
+    IP,
+    PORT,
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    MESSAGE
+}
