6d32e4a49b67fcd92992c126d5dc27d1355fc1ad
porchsal <porchsal@yahoo.com>
2023-03-26 01:47:42 -0400

    play.js upload

--- play.js
+++ play.js
@@ -1,23 +1,23 @@
 const net = require("net");
 const { IP, PORT } = require("./constants.js");
-const { setupInput } = require("./input.js")
+const { setupInput } = require("./input.js");
 
 
 // establishes a connection with the game server
-const connect = function () {
-    const conn = net.createConnection({
-        host : IP,
-        port : PORT,
-    });
-// Interpret incoming data as text
-    conn.setEncoding("utf8");
-    conn.on("connect", () => {
-        conn.write("Name: JSE");
-    })
-    conn.on("connect", () => {
-        console.log("Successfully connected to game server");
-    })
-    return conn;
+const connect = function() {
+  const conn = net.createConnection({
+    host : IP,
+    port : PORT,
+  });
+  // Interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    conn.write("Name: JSE");
+  });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+  return conn;
 };
 
 // setup interface to handle user input from stdin
