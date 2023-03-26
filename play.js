2341f173a4d590c121bcc00311ebe22b1d6175fc
porchsal <porchsal@yahoo.com>
2023-03-25 22:42:34 -0400

    play.js ctrl+c

--- play.js
+++ play.js
@@ -25,6 +25,24 @@ const connect = function () {
     return conn;
 };
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+  };
+
+const handleUserInput = function(){
+    if (key === '\u0003') {
+        process.exit();
+      }
+};
+
+
 console.log("Connecting....");
 connect();
 
