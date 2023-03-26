6847a9ecd186f50a251c5c9aceb6b681ff7837ac
porchsal <porchsal@yahoo.com>
2023-03-26 01:47:21 -0400

    input.js upload

--- input.js
+++ input.js
@@ -2,35 +2,35 @@ const { MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, MESSAGE } = require("./consta
 
 let connection;
 //function to catch keyboard
-const setupInput = function (conn) {
-    connection = conn;
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
     
-    return stdin;
-  };
+  return stdin;
+};
 
 //movements sent by user through keyboard
-const handleUserInput = function(key){
-    if (key === '\u0003') {
-        process.exit();
-      } else if (key === MOVE_UP){
-        connection.write("Move: up");
-      } else if ( key === MOVE_DOWN){
-        connection.write("Move: down");
-      } else if ( key === MOVE_LEFT){
-        connection.write("Move: left");
-      } else if ( key === MOVE_RIGHT){
-        connection.write("Move: right");
-      } else if ( key === MESSAGE){
-        console.log("SNAKESSSSSS!")
-      }
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === MOVE_UP) {
+    connection.write("Move: up");
+  } else if (key === MOVE_DOWN) {
+    connection.write("Move: down");
+  } else if (key === MOVE_LEFT) {
+    connection.write("Move: left");
+  } else if (key === MOVE_RIGHT) {
+    connection.write("Move: right");
+  } else if (key === MESSAGE) {
+    console.log("SNAKESSSSSS!");
+  }
 };
 
 
-  module.exports = {
-    setupInput
-  };
+module.exports = {
+  setupInput
+};
