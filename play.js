454d5edac43664c1df80c4819eb441c1e9f28043
porchsal <porchsal@yahoo.com>
2023-03-26 01:33:09 -0400

    play.js constants

--- play.js
+++ play.js
@@ -1,29 +1,21 @@
 const net = require("net");
+const { IP, PORT } = require("./constants.js");
 const { setupInput } = require("./input.js")
-//const connect = require('./client.js');
 
 
 // establishes a connection with the game server
 const connect = function () {
     const conn = net.createConnection({
-        host : 'localhost',
-        port : 50541,
+        host : IP,
+        port : PORT,
     });
 // Interpret incoming data as text
     conn.setEncoding("utf8");
     conn.on("connect", () => {
         conn.write("Name: JSE");
-//        conn.write("Move: Up");
     })
     conn.on("connect", () => {
         console.log("Successfully connected to game server");
-        // conn.write("Move: Up");
-        // setInterval(() => {
-        //     conn.write("Move: Up");
-        // }, 50)
-    })
-    conn.on("data", () => {
-        
     })
     return conn;
 };
@@ -34,7 +26,6 @@ const connect = function () {
 
 
 
-console.log("Connecting....");
-//connect();
+//console.log("Connecting....");
 
 setupInput(connect());
